da559c31ca2587eee1cbac44ace3a2c3afa2a335
Asutosh Palai <asupalai@gmail.com>
2018-03-29 13:07:42 +0530

    Added dnust config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 shopt -s extglob
-ln -s $(realpath $(dirname "$0"))/*/!(..|.) ~/
+ln -s $(realpath $(dirname "$0"))/!(config)/!(config|..|.) ~/
+mkdir -p ~/.config
+ln -s $(realpath $(dirname "$0"))/config/!(..|.) ~/.config
 
 cd ~/.fzf && ./install --all
 vim +PlugInstall +qall
